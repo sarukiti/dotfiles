edfb2487b67d6d4080537011dc2cf97bf06eeeaf
sarukiti <sarukiti1891@gmail.com>
2026-04-23 15:33:59 +0900

    補完きくぞ

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -17,8 +17,15 @@ return {
   },
   {
     "williamboman/mason-lspconfig.nvim",
-    dependencies = { "williamboman/mason.nvim", "neovim/nvim-lspconfig" },
+    dependencies = {
+      "williamboman/mason.nvim",
+      "neovim/nvim-lspconfig",
+      "hrsh7th/cmp-nvim-lsp",
+    },
     config = function()
+      vim.lsp.config("*", {
+        capabilities = require("cmp_nvim_lsp").default_capabilities(),
+      })
       require("mason-lspconfig").setup({
         ensure_installed = {
           "lua_ls",
